f9c3eaf09e32556057b819d056c8b7b0fa52761f
succhi111 <suchendramishra396@gmail.com>
2022-10-21 20:23:38 +0530

    Initial commit

---
+++ src/main/java/EmployeePayrollDB.java
@@ -0,0 +1,47 @@
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class EmployeePayrollDB {
+    public static void main(String[] args) {
+
+        /**
+         * jdbc connection with sql
+         */
+        String jdbcURL = "jdbc:mysql://localhost:3306/employeepayrolljdbc";
+        /**
+         * sql username
+         */
+        String userName = "root";
+        /**
+         * sql password
+         */
+        String password = "Srushti@1999";
+        listDrivers();
+        try {
+            System.out.println("Connecting to Database: " + jdbcURL);
+            Connection connection = DriverManager.getConnection(jdbcURL, userName, password);
+            System.out.println("Connection is successful !!1: " + connection);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * create a method name as listDrivers
+     */
+    private static void listDrivers() {
+        /**
+         * Enumeration :-
+         *
+         *  An object that implements the Enumeration interface generates a series of elements, one at a time.
+         *  Successive calls to the nextElement method return successive elements of the series.
+         */
+        Enumeration<Driver> driverList = DriverManager.getDrivers();
+        while (driverList.hasMoreElements()) {
+            Driver driverClass = (Driver) driverList.nextElement();
+            System.out.println("  " + driverClass.getClass().getName());
+        }
+    }
+}
